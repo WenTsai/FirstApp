bcd6068cb16df8ef94692aa3677fd6c7b05c4f1d
我 <Wen-Lin@wenlintsaidemacbook-pro.local>
2016-03-16 19:34:21 +0800

    Initial Commit

---
+++ class0316Tests/class0316Tests.swift
@@ -0,0 +1,36 @@
+//
+//  class0316Tests.swift
+//  class0316Tests
+//
+//  Created by WenLin on 2016/3/16.
+//  Copyright © 2016年 WenLin. All rights reserved.
+//
+
+import XCTest
+@testable import class0316
+
+class class0316Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
